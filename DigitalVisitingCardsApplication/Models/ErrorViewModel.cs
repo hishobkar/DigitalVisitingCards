10c083220148451f07026a9e17a944c677613e97
hishobkar <abhishobkar@gmail.com>
2023-08-20 15:20:47 +0530

    initial directory structure for MVC project

---
+++ DigitalVisitingCardsApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DigitalVisitingCardsApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
